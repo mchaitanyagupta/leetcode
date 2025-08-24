4d4283e10836c76b68aeffadbafd35af516933ff
MUVVALA  NAGA CHAITANYA GUPTA <112788162+mchaitanyagupta@users.noreply.github.com>
2025-08-24 07:39:50 +0530

    reverse a char array using recursion

---
+++ Recursion/reverse a char array using recursion.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    //using recursion
+    public void reverseString(char[] s) {
+        helper(s, 0, s.length - 1);
+    }
+    public static void helper(char[] s, int left, int right){
+        if(left >= right)return;
+        char c = s[left];
+        s[left] = s[right];
+        s[right] = c;
+        helper(s, left+1, right - 1);
+    }
+}
